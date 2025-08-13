0dec48c4f4f9ffd9f35a55c5773de158e77c648d
Evan Black <evan@evan.black>
2025-08-13 19:41:41 -0400

    Add plots for clones & views

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 import sqlite3
 import enum
 import datetime
+import calendar
 
 
 class ProjectCode(enum.IntEnum):
@@ -41,6 +42,18 @@ def index():
     clones = c.fetchall()
 
     c.execute("""
+              select sum(clone_count)                       as 'clone_count',
+                     sum(unique_clone_count)                as 'unique_clone_count',
+                     strftime('%m-%Y', clone_day)           as 'month_year',
+                     cast(strftime('%m', clone_day) as int) as 'month',
+                     strftime('%Y', clone_day)              as 'year'
+              from clones
+              group by strftime('%m-%Y', clone_day)
+              order by clone_day;
+              """)
+    clones_by_month = c.fetchall()
+
+    c.execute("""
               select project_id,
                      sum(view_count)        as total_views,
                      sum(unique_view_count) as total_unique_views
@@ -51,6 +64,18 @@ def index():
 
     views = c.fetchall()
 
+    c.execute("""
+              select sum(view_count)                       as 'view_count',
+                     sum(unique_view_count)                as 'unique_view_count',
+                     strftime('%m-%Y', view_day)           as 'month_year',
+                     cast(strftime('%m', view_day) as int) as 'month',
+                     strftime('%Y', view_day)              as 'year'
+              from views
+              group by strftime('%m-%Y', view_day)
+              order by view_day;
+              """)
+    views_by_month = c.fetchall()
+
     total_clones = {
         'clones': 0,
         'unique_clones': 0,
@@ -59,7 +84,6 @@ def index():
     for r in clones:
         total_clones['clones'] += r['total_clones']
         total_clones['unique_clones'] += r['total_unique_clones']
-
 
     total_views = {
         'views': 0,
@@ -70,7 +94,6 @@ def index():
         total_views['views'] += v['total_views']
         total_views['unique_views'] += v['total_unique_views']
 
-
     c.close()
     conn.close()
     return render_template("index.html",
@@ -79,7 +102,11 @@ def index():
                            projectCode=ProjectCode,
                            target_date=target,
                            total_clones=total_clones,
-                           total_views=total_views)
+                           clones_by_month=clones_by_month,
+                           total_views=total_views,
+                           views_by_month=views_by_month,
+                           month_names=calendar.month_name
+                           )
 
 
 if __name__ == '__main__':
