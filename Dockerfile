a601bb9ad18166e071c053b997f1ccdf9b17eed8
Evan Black <evan@evan.black>
2025-08-13 09:45:45 -0400

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,25 @@
+FROM archlinux:latest
+
+RUN pacman -Syu --noconfirm && \
+    pacman -S --noconfirm python python-pip && \
+    pacman -Scc --noconfirm
+
+WORKDIR /app
+COPY requirements-prod.txt .
+
+RUN python -m venv /app/venv && \
+    /app/venv/bin/pip install --upgrade pip && \
+    /app/venv/bin/pip install --no-cache-dir -r requirements-prod.txt
+
+COPY . .
+
+VOLUME ["/app/data"]
+
+RUN useradd -m -s /bin/bash flaskuser && \
+    chown -R flaskuser:flaskuser /app
+USER flaskuser
+
+EXPOSE 8000
+
+# Run Flask application
+CMD ["/app/venv/bin/gunicorn", "--bind", "0.0.0.0:8000", "--workers", "4", "app:app"]
---
+++ app.py
@@ -0,0 +1,86 @@
+from flask import Flask, render_template
+import sqlite3
+import enum
+import datetime
+
+
+class ProjectCode(enum.IntEnum):
+    NetsimulyzerApp = 1
+    NetsimulyzerModule = 2
+
+
+app = Flask(__name__)
+
+
+def connect_db() -> sqlite3.Connection:
+    connection = sqlite3.connect("data/clone-stats.sqlite")
+    connection.row_factory = sqlite3.Row
+    return connection
+
+
+@app.route('/')
+def index():
+    d = datetime.date.today()
+    end_of_fy = datetime.date(year=d.year, month=10, day=1)
+
+    if d > end_of_fy:
+        target = end_of_fy
+    else:
+        target = datetime.date(year=d.year - 1, month=10, day=1)
+
+    conn = connect_db()
+    c = conn.cursor()
+    c.execute("""
+              select project_id,
+                     sum(clone_count)        as total_clones,
+                     sum(unique_clone_count) as total_unique_clones
+              from clones
+              where clone_day > ?
+              group by project_id;
+              """, (target,))
+    clones = c.fetchall()
+
+    c.execute("""
+              select project_id,
+                     sum(view_count)        as total_views,
+                     sum(unique_view_count) as total_unique_views
+              from views
+              where view_day > ?
+              group by project_id;
+              """, (target,))
+
+    views = c.fetchall()
+
+    total_clones = {
+        'clones': 0,
+        'unique_clones': 0,
+    }
+
+    for r in clones:
+        total_clones['clones'] += r['total_clones']
+        total_clones['unique_clones'] += r['total_unique_clones']
+
+
+    total_views = {
+        'views': 0,
+        'unique_views': 0,
+    }
+
+    for v in views:
+        total_views['views'] += v['total_views']
+        total_views['unique_views'] += v['total_unique_views']
+
+
+    c.close()
+    conn.close()
+    return render_template("index.html",
+                           clones=clones,
+                           views=views,
+                           projectCode=ProjectCode,
+                           target_date=target,
+                           total_clones=total_clones,
+                           total_views=total_views)
+
+
+if __name__ == '__main__':
+    app.run()
